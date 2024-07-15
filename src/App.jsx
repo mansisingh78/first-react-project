f83d4525de612ee76d3679dcd819cd97598a214a
Mansi singh <mansi123singh1234@gmail.com>
2024-07-15 21:45:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="nav">
+        <div className="left">
+          <h3>Encore</h3>
+        </div>
+        <div className="right">
+          <ul>
+            <li>HOME</li>
+            <li>EVENTS</li>
+            <li>SCHEDULE</li>
+            <li>VENUE</li>
+            <li>REGISTER</li>
+          </ul>
+        </div>
+      </div>
+      <div className="container">
+        <h1>Dedicated to Altcampus friends and family</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus
+          quis inventore incidunt, <br />
+          fuga ipsa quas?
+        </p>
+        <button>Register Now</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
